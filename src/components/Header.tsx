35ac1f1f0cc0713b8c091f53e9d324b284f8c136
Manuel Figueira <manuel.figueira.9@gmail.com>
2024-06-10 15:40:42 +0900

    add rainbowkit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
 export const Header = () => {
   return (
-    <header>
+    <header className="flex justify-between p-6">
       <h1>My App</h1>
+
+      <ConnectButton />
     </header>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,30 @@
 import "~/styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+
 import type { AppProps } from "next/app";
 
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { bscTestnet, sepolia } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const config = getDefaultConfig({
+  appName: process.env.NEXT_PUBLIC_WC_APP_NAME || "",
+  projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID || "",
+  chains: [bscTestnet, sepolia],
+  // ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
 }
